591874cb145b5b141743540f7f18410b576ad545
Osouzaa <gabrielsouzaalves750@gmail.com>
2025-01-07 10:29:41 -0300

    feat: updateds

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { darkTheme } from '../styles/theme/darkTheme'
 import { lightTheme } from '../styles/theme/lightTheme'
 import { GlobalStyle } from '../styles/global'
 import { Header } from '../components/header'
-import { AppContainer } from './styles'
+import { AppContainer, ScrollButton } from './styles'
 import { Banner } from '../components/banner'
 import { About } from '../components/about'
 import { Skills } from '../components/skills'
@@ -13,11 +13,12 @@ import { Work } from '../components/work'
 import { Footer } from '../components/footer'
 import { Form } from '../components/form'
 import { Toaster } from 'sonner'
+import { FaLongArrowAltUp } from 'react-icons/fa'
 
 export function App() {
   const [theme, setTheme] = useState<'dark' | 'light'>('dark')
+  const [showScrollButton, setShowScrollButton] = useState(false)
 
-  // Criando referências para cada seção
   const homeRef = useRef<HTMLDivElement>(null)
   const aboutRef = useRef<HTMLDivElement>(null)
   const skillsRef = useRef<HTMLDivElement>(null)
@@ -30,6 +31,18 @@ export function App() {
   const handleTheme = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark')
   }
+
+  const scrollToTop = () => {
+    homeRef.current?.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowScrollButton(window.scrollY > 200)
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
 
   return (
     <ThemeProvider theme={currentTheme}>
@@ -66,6 +79,14 @@ export function App() {
         <Form />
       </div>
       <Footer />
+      {showScrollButton && (
+        <ScrollButton
+          onClick={scrollToTop}
+          title="Volta para o inicio da Pagina"
+        >
+          <FaLongArrowAltUp size={20} />
+        </ScrollButton>
+      )}
     </ThemeProvider>
   )
 }
--- src/app/styles.ts
+++ src/app/styles.ts
@@ -9,3 +9,25 @@ export const AppContainer = styled.main`
     padding: 1rem;
   }
 `
+
+export const ScrollButton = styled.button`
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  background-color: ${({ theme }) => theme.primary};
+  color: ${({ theme }) => theme.default};
+  border: none;
+  border-radius: 50%;
+  padding: 10px 13px;
+  font-size: 18px;
+  cursor: pointer;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
+  transition: background-color 0.3s;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &:hover {
+    background-color: ${({ theme }) => theme.secondary};
+  }
+`
